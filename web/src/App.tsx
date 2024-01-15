31fd7cc90bfb36372570b595c9e8141b01bb9c7f
Gabriel Baierle <gbaierle92@gmail.com>
2024-01-15 17:38:32 -0300

    create template React app with Vite

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white">
+      <h1>Hello Vite + React!</h1>
+    </div>
+  )
+}
+
+export default App
