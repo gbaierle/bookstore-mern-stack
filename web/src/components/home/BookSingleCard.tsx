e9e83529441703f539ba6826b30f689afeb6dc51
Gabriel Baierle <gbaierle92@gmail.com>
2024-01-15 20:57:51 -0300

    add book modal

--- web/src/components/home/BookSingleCard.tsx
+++ web/src/components/home/BookSingleCard.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import { AiOutlineEdit } from "react-icons/ai";
-import { BiUserCircle } from "react-icons/bi";
+import { BiShow, BiUserCircle } from "react-icons/bi";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import BookModal from "./BookModal";
 
 type Book = {
   _id: string;
   title: string;
   author: string;
   publishYear: number;
-}
+};
 
 const BookSingleCard = ({ book }: { book: Book }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
@@ -28,6 +32,10 @@ const BookSingleCard = ({ book }: { book: Book }) => {
         <h2 className="my-1">{book.author}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+          className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+          onClick={() => setShowModal(true)}
+        />
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
         </Link>
@@ -38,6 +46,9 @@ const BookSingleCard = ({ book }: { book: Book }) => {
           <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
         </Link>
       </div>
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 };
